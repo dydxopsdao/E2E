f4ba7ac0d2d54555d314cba485034689821e124a
ben-dydx-ops <ben@dydxopsservices.com>
2025-02-27 13:24:13 +0100

    feat: aplitools ci

--- config/applitools.config.ts
+++ config/applitools.config.ts
@@ -6,18 +6,13 @@ import {
 } from "@applitools/eyes-playwright";
 require("dotenv").config({ path: process.env.ENV_PATH || ".env" });
 
-if (!process.env.APPLITOOLS_BATCH_ID) {
-  // If you truly want to enforce the variable, keep throwing:
-  throw new Error("APPLITOOLS_BATCH_ID must be set for a stable batch ID.");
-}
-
 export const runner = new ClassicRunner();
 export const config = new Configuration();
 
 config.setApiKey(process.env.APPLITOOLS_API_KEY || "");
 config.setServerUrl(process.env.APPLITOOLS_SERVER_URL || "https://eyesapi.applitools.com");
 
-const batchId = process.env.APPLITOOLS_BATCH_ID;
+const batchId = process.env.APPLITOOLS_BATCH_ID || '';
 const batch = new BatchInfo(`DYDX-${batchId}`);
 batch.setId(batchId);
 config.setBatch(batch);
