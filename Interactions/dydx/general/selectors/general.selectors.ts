c2096fe68949f38b09528f05e94b4b622b086a16
ben-dydx-ops <ben@dydxopsservices.com>
2025-05-22 10:31:52 +0200

    Update general.selectors.ts

--- Interactions/dydx/general/selectors/general.selectors.ts
+++ Interactions/dydx/general/selectors/general.selectors.ts
@@ -1,6 +1,4 @@
 export const GeneralSelectors = {
   closeButton: '[role="dialog"] button > svg.sc-17s7t24-0.wZEdK',
-  // Wallet address is identified by both classes on the same element
-  // so they should be concatenated without a space
   walletAddress: ".sc-46k8ig-7.cGfQrQ",
 };
