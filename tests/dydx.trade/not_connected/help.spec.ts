e0ea873a19343d75a5bad1d44715e4695036104f
Ben Wallace <ben@dydxopsservices.com>
2025-06-05 10:04:23 +0200

    feat:test help

---
+++ tests/dydx.trade/not_connected/help.spec.ts
@@ -0,0 +1,16 @@
+import { eyesTest as test } from "@fixtures/eyesFixture";
+import { navigateToDydxPage } from "@interactions/dydx/general/actions/navigation.actions";
+import { expect, Page } from "@playwright/test";
+import { logger } from "@utils/logger/logging-utils";
+import { visualCheck } from "@utils/visual-check";
+
+test.describe('help', () => {
+  test('help', async ({ page, eyes }) => {
+    await navigateToDydxPage(page, "/portfolio/overview");
+    await page.click('//*[@id="root"]/div/header/div[6]/button[3')
+    await expect(page.locator('.sc-18mw14j-2]')).toBeVisible();
+    await visualCheck(eyes, { name: 'help', useDom: true });
+      logger.success(`Completed visual check for help`);
+
+  });
+});
