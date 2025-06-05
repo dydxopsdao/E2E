42109a2a935505c5fbafd27a0e9fe4d8522344e5
Ben Wallace <ben@dydxopsservices.com>
2025-06-05 14:43:43 +0100

    chore: fix selectors

--- tests/dydx.trade/not_connected/help.spec.ts
+++ tests/dydx.trade/not_connected/help.spec.ts
@@ -5,10 +5,12 @@ import { logger } from "@utils/logger/logging-utils";
 import { visualCheck } from "@utils/visual-check";
 
 test.describe('help', () => {
+  test.describe.configure({ retries: 0 });
   test('help', async ({ page, eyes }) => {
+    
     await navigateToDydxPage(page, "/portfolio/overview");
-    await page.click('//*[@id="root"]/div/header/div[6]/button[3')
-    await expect(page.locator('.sc-18mw14j-2]')).toBeVisible();
+    await page.click('//*[@id="root"]/div/header/div[6]/button[2]', { force: true });
+    await expect(page.locator('.sc-18mw14j-2')).toBeVisible();
     await visualCheck(eyes, { name: 'help', useDom: true });
       logger.success(`Completed visual check for help`);
 
